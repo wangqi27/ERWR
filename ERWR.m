8d3a4cb43f65c2747f30f3fe53c7967016321f03
wangqi27 <46438921+wangqi27@users.noreply.github.com>
2019-09-27 09:50:07 +0800

    Add files via upload

---
+++ ERWR.m
@@ -0,0 +1,18 @@
+%input:adjacency matrix A
+%output:efficacious score S
+function S=ERWR(A)
+c=0.05;%restart probability
+[cc,cc]=size(A);
+w=inv(diag(sum(A,2)))*A;
+for k=1:cc
+p0=zeros(cc,1);
+p0(k)=1; 
+p1=p0;
+p(:,k)=(1-c)*w'*p0+c*p0;
+while max(abs(p(:,k)-p1))>10^(-6)
+    p1=p(:,k);
+    p(:,k)=(1-c)*w'*p1+c*p0;
+end
+end
+S=p+p';
+end
